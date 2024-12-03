45414bb4eb7c3b00884f0794399c4bb4ac566530
raydon <815718086@qq.com>
2024-12-03 20:41:24 +0000

    Refresh the figure window

--- solution2D.m
+++ solution2D.m
@@ -10,28 +10,41 @@ T = T0 + dTdz_boundaries(2)*Zc;  % initialise T array (based on what emily wrote
 dt = CFL * (h/2)^2 / max(k0, [], "all"); % time step [s]
 
 air = units == 9;% indices of air coordinates
+k = 100;  % 时间步长增量的乘数
+t = 0;    % 初始时间 [s]
+tau = 0;  % 迭代步数计数
+% 创建图形窗口
+figure;
 
-k=100;
-for t = 0:(dt*k):t_end
-    % Calculate temperature changes using the diffusion function
+while t <= t_end
+    % canculate the temp
     R1 = diffusion(T, k0, h, [1, 1:Nx, Nx], [1, 1:Nz, Nz], dTdz_boundaries(2));
     R2 = diffusion(T + R1 * dt / 2, k0, h, [1, 1:Nx, Nx], [1, 1:Nz, Nz], dTdz_boundaries(2));
     R3 = diffusion(T + R2 * dt / 2, k0, h, [1, 1:Nx, Nx], [1, 1:Nz, Nz], dTdz_boundaries(2));
     R4 = diffusion(T + R3 * dt, k0, h, [1, 1:Nx, Nx], [1, 1:Nz, Nz], dTdz_boundaries(2));
 
-    % Update temperature with Runge-Kutta scheme
+    % Runge-Kutta
     T = T + dt * (R1 + 2 * R2 + 2 * R3 + R4) / 6 + Hr ./ (rho .* Cp);
+
+    % time and step
+    t = t + dt * k;  % add dt*k every time
+    tau = tau + 1;
+
+   % Visualize the temperature distribution every 'nop' steps
+    if mod(tau, nop) == 0
+        clf;%clear the fig
+
+        imagesc(x_cells, z_cells, T); % Plot temperature distribution
+        colorbar;
+        t_years = t / yr;  % Calculate current time in years
+
+        title(['Temperature Distribution at t = ' num2str(t_years) ' years']);% Update title with years
+        xlabel('Lateral Distance [m]');
+        ylabel('Depth [m]');   
+        clim([min(T(:)), max(T(:))]);  % Set the color bar's min and max range
+        drawnow;  % Refresh the figure window
+    end
 end
-
-% 6. Visualize the temperature field
-imagesc(x_cells, z_cells, T);  % Display temperature field
-colorbar;
-title('Temperature Distribution Simulation Result');
-xlabel('Lateral Distance [m]');
-ylabel('Depth [m]');
-
-% Save the final temperature field
-save('thermal_distribution.mat', 'T');
 
 % Auxiliary functions
 function dTdt = diffusion(T, k0, h, ix, iz, base_flux)
